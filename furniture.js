71404da2431c2fb28b5cffddd828019b742a2e5f
soniaaditya <156799564+soniaaditya@users.noreply.github.com>
2024-01-20 18:07:03 +0530

    Add files via upload

---
+++ furniture.js
@@ -0,0 +1,14 @@
+function first(){
+    document.getElementById("sliderimage").src="file:///C:/Users/JAYESH/Pictures/Camera%20Roll/3_luxury-classic-Italian-empire-dining-room-furniture-handmade-solid-wood-best-home-furnishing-projects-min.jpg";
+}
+function second(){
+    document.getElementById("sliderimage").src="file:///C:/Users/JAYESH/Pictures/Camera%20Roll/darwazehliving-italian-hero.jpg";
+}
+function third(){
+    document.getElementById("sliderimage").src="file:///C:/Users/JAYESH/Pictures/Camera%20Roll/images%20(4).jpg";
+}
+
+setInterval(first, 1800);
+setInterval(second, 3600);
+setInterval(third, 5400);
+
